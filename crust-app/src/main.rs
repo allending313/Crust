7d21d950aa18547bab2d66a9fd2059ab84649099
Allen Ding <allending313@gmail.com>
2022-09-02 23:41:30 -0500

    sample html page

--- crust-app/src/main.rs
+++ crust-app/src/main.rs
@@ -1,4 +1,4 @@
-use axum::{routing::get, Router};
+use axum::{response::Html, routing::get, Router};
 use std::net::SocketAddr;
 
 #[tokio::main]
@@ -17,6 +17,8 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler() -> &'static str {
-    "Hello, world!"
+async fn handler() -> Html<&'static str> {
+    // `std::include_str` macro can be used to include an utf-8 file as `&'static str` in compile
+    // time. This method is relative to current `main.rs` file.
+    Html(include_str!("../templates/index.html"))
 }
